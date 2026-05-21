89c7c47dbe169dba041a7dd11652e328b8f4296a
cheemsup <cheemsup@outlook.com>
2026-05-21 08:54:33 +0800

    init

---
+++ youxx_backend/src/main/java/org/youxx/YouxxApplication.java
@@ -0,0 +1,13 @@
+package org.youxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouxxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouxxApplication.class, args);
+    }
+
+}
---
+++ youxx_backend/src/test/java/org/youxx/YouxxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouxxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
